923a4a07194e2cd580b99ebc7df3a563d710bae6
Yous77130 <13012@holbertonstudents.com>
2026-06-19 17:33:51 +0200

    Task 0: wait_random coroutine

--- python_async_function/0-basic_async_syntax.py
+++ python_async_function/0-basic_async_syntax.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-"""Module qui fournit une coroutine asynchrone de délai aléatoire."""
+"""Module fournissant une coroutine asynchrone de délai aléatoire."""
 import asyncio
 import random
 
 
 async def wait_random(max_delay: int = 10) -> float:
-    """Attend un délai aléatoire entre 0 et max_delay secondes et le retourne."""
+    """Attend un délai aléatoire entre 0 et max_delay et le retourne."""
     delay = random.uniform(0, max_delay)
     await asyncio.sleep(delay)
     return delay
