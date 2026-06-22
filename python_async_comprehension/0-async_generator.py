d9c055c91d39ea1ecc21325a24f22d121d26f437
Yous77130 <13012@holbertonstudents.com>
2026-06-22 11:38:23 +0200

    Fix: use Generator type-hint for async_generator

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -2,10 +2,10 @@
 """Module qui fournit un generateur asynchrone de nombres aleatoires."""
 import asyncio
 import random
-from typing import AsyncGenerator
+from typing import Generator
 
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> Generator[float, None, None]:
     """Boucle 10 fois, attend 1s et yield un nombre entre 0 et 10."""
     for _ in range(10):
         await asyncio.sleep(1)
